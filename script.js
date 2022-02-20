587b0a2ad3d75458e6f15ef447d6d9993a873d2e
Damian <damian.mrozek@vp.pl>
2022-02-20 19:05:20 +0100

    zamknięcie ClearErrorMessage do funkcji

--- script.js
+++ script.js
@@ -13,35 +13,36 @@ function showErrorMessage(input, message){
     errorMessage.textContent = message;
 }
 
-function checkPasswordsValue(password1, password2){
-    const box = password2.parentElement;
+function clearErrorMessage(input){
+    const box = input.parentElement;
     const errorMessage = box.querySelector('.error');
+
+    errorMessage.style.display = 'none';
+}
+
+function checkPasswordsValue(password1, password2){
     
     if(password1.value !== password2.value){
         showErrorMessage(password2, 'Podane hasła są różne!');
     } else {
-        errorMessage.style.display = 'none';
+        clearErrorMessage(password2);
     }
 }
 
 function checkInputLength(input, minValue){
-    const box = input.parentElement;
-    const errorMessage = box.querySelector('.error');
 
     if(input.value.length < minValue){
         showErrorMessage(input, `Pole powinno zawierać ${minValue} znaków`);
     } else {
-        errorMessage.style.display = 'none';
+        clearErrorMessage(input);
     }
 }
 
 function checkEmailValue(email){
-    const box = email.parentElement;
-    const errorMessage = box.querySelector('.error');
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if(regex.test(email.value)){
-        errorMessage.style.display = 'none';
+        clearErrorMessage(email);
     } else {
         showErrorMessage(email, 'E-mail niepoprawny');
     }
@@ -51,9 +52,7 @@ clearButton.addEventListener('click', e => {
     e.preventDefault();
     [userName, password1, password2, email].forEach(input => {
         input.value = '';
-        const box = input.parentElement;
-        const errorMessage = box.querySelector('.error');
-        errorMessage.style.display = 'none';
+        clearErrorMessage(input);
     })
 })
 
