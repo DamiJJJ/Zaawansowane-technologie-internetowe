5027b31e22ef77a51cdd145655213579333b303e
Damian <damian.mrozek@vp.pl>
2022-03-06 11:47:23 +0100

    weather init

---
+++ weatherjs.js
@@ -0,0 +1,19 @@
+// obiekty
+
+// const dog = {
+//     name : 'Max',
+//     age : 5,
+//     'eye color' : 'black'
+// }
+
+// console.log(dog);
+// console.log(typeof(dog));
+
+
+// JSON
+
+// {
+//     "lokalizacja" : "Polska",
+//     "stolica" : "Warszawa",
+//     "populacja" : 32000000
+// }
